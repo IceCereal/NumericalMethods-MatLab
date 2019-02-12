34e6dd38e2c97a41e6d34e0ea6627d506b46cae8
Raghav NS <35899273+IceCereal@users.noreply.github.com>
2019-02-12 16:06:25 +0530

    Update Dolittle.m
    Include Primary Methods

--- LU_Decompostion/Dolittle.m
+++ LU_Decompostion/Dolittle.m
@@ -14,3 +14,14 @@ end
 %Create LU
 L = zeros(n, n);
 U = zeros(n, n);
+
+%Init L Diagnol to be 1
+for i = 1:n
+    L(i, i) = 1;
+end
+
+%Method of figuring out U(1, *)
+U(1,:) = A(1,:);
+
+%Method of figuring out L(*, 1)
+L(2:n, 1) = A(2:n, 1) / U(1, 1)
